0de08865d1c16e5742f7a061250bbe3a57a5ab40
Eddie <ehwa009@aucklanduni.ac.nz>
2018-09-18 10:13:56 +1200

    add bar graph function

--- src/node.py
+++ src/node.py
@@ -6,18 +6,28 @@ import matplotlib.pyplot as plt
 import random
 import rospkg
 import os
+import numpy as np
 
-from mind_msgs.msg import LearningFigure
+from mind_msgs.msg import LearningFigure, ResponseProb
+from scipy.interpolate import spline
 
 class Plot():
 
     def __init__(self):
         
+        self.path = os.path.join(
+                rospkg.RosPack().get_path('learning_plotting_ros'), 'figure')
+        
+        # For learning plot
         self.iteration = []
         self.accuracy = []
         self.loss = []
 
+        # For response probailities graph
+        self.fig_number = 1    
+
         rospy.Subscriber('learning', LearningFigure, self.handle_update)
+        rospy.Subscriber('probs', ResponseProb, self.handle_probs_update)
         rospy.loginfo('\033[94m[%s]\033[0m initialized.'%rospy.get_name())
 
     def handle_update(self, msg):
@@ -43,14 +53,40 @@ class Plot():
             plt.xlim([0, len(self.iteration)])
             plt.ylim([0, self.loss[0]])
             
-            path = os.path.join(
-                rospkg.RosPack().get_path('learning_plotting_ros'), 'figure')
-            
             # save learning figure
-            # plt.savefig("%s/learning.png"%path, dpi=350)
+            # plt.savefig("%s/learning.png"%self.path, dpi=350)
             plt.show()
 
             rospy.signal_shutdown('record completed.')
+    
+    def handle_probs_update(self, msg):
+        if msg.resp_prob != []:
+            y_probs = msg.resp_prob
+            x_name = [i for i, p in enumerate(y_probs)]
+            n_groups = len(x_name)
+
+            index = np.arange(n_groups)
+            bar_width = 0.35
+            opacity = 0.5  
+
+            plt.bar(index, y_probs, bar_width,
+                    tick_label=x_name, align='center',
+                    alpha=opacity, color='b', label='probs')
+
+            plt.xlabel('Responses on the action template')
+            plt.ylabel('Probability')
+            plt.title('Response Space Distribution')
+            plt.xlim(-1, n_groups)
+            plt.ylim(0, 1)
+
+            # save learning figure
+            plt.savefig("%s/prob_fig %i.png"%(self.path, self.fig_number), dpi=350)
+            self.fig_number += 1
+
+            plt.show()
+        
+        else:
+            rospy.logerr('no data coming from ...')
 
 
 if __name__ == '__main__':
